a3e266d168dabe41806a3d1bd23075a6cb47a9c1
jammc89 <jammc89@gmail.com>
2025-02-02 11:32:40 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import InsuranceVerificationApp from '../components/InsuranceVerificationApp';
+import InsuranceVerificationApp from './InsuranceVerificationApp';
 
 export default function Home() {
   return (
