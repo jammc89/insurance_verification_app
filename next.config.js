1b28caa026934fa473e31569d06ef03034dfba82
jammc89 <jammc89@gmail.com>
2025-02-02 11:30:21 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    esmExternals: true
+  },
+  webpack: (config) => {
+    config.resolve.extensions = ['.js', '.jsx', '.json'];
+    return config;
+  }
 }
 
 module.exports = nextConfig
