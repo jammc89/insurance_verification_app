f5f37cd556716edd91495ffc483f044d466dc26c
jammc89 <jammc89@gmail.com>
2025-02-02 10:30:54 -0600

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,11 @@
+'use client';
+
+import InsuranceVerificationApp from '../components/InsuranceVerificationApp';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50 py-8">
+      <InsuranceVerificationApp />
+    </main>
+  );
+}
