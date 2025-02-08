e6cc470b692548cf9ca5ccdff2edbf7fb1414afb
jammc89 <jammc89@gmail.com>
2025-02-07 18:55:51 -0600

    Update VerificationResults.js

--- components/VerificationResults.js
+++ components/VerificationResults.js
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';  // Added useState import
 import CostCalculator from './CostCalculator';
 
 const mockVerificationData = {
