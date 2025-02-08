f342bcce1421e21102d139c54c2742ad09fbc83a
jammc89 <jammc89@gmail.com>
2025-02-08 12:00:52 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,9 @@
 'use client';
 
-import InsuranceVerificationApp from './InsuranceVerificationApp';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import LoginPage from './auth/login/page';
 
 export default function Home() {
-  return (
-    <main className="min-h-screen bg-gray-50 py-8">
-      <InsuranceVerificationApp />
-    </main>
-  );
+  return <LoginPage />;
 }
